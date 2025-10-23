158c54504acc773b6fa45aa155d9e7e51efc5eef
SoyWadyRD <andreslopez261316@gmail.com>
2025-10-23 18:47:58 -0400

    Tu mensaje de commit aquí

--- backend/utils/enviarCorreo.js
+++ backend/utils/enviarCorreo.js
@@ -1,10 +1,8 @@
-const nodemailer = require("nodemailer");
-
 const enviarCorreo = async (to, subject, html) => {
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
-    secure: false,
+    secure: false, // Usa true si el puerto es 465
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS
@@ -21,8 +19,7 @@ const enviarCorreo = async (to, subject, html) => {
     console.log(`Correo de verificación enviado a ${to}`);
   } catch (error) {
     console.error(`Error al enviar el correo: ${error.message}`);
+    res.status(500).json({ mensaje: "Error al enviar el correo de verificación", error: error.message });
     throw new Error("No se pudo enviar el correo de verificación.");
   }
 };
-
-module.exports = enviarCorreo;
